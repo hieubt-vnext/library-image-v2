5dd7c6735fa3588b622a19abdec308b5d089f205
hieubt <hieubt@vnext.vn>
2025-09-19 14:58:24 +0700

    change size image viewer

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import { ZoomPanPinchViewer } from "@/components/zoom-pan-pinch-viewer"
 
 export default function Home() {
   return (
-    <main className="h-full bg-background flex flex-col overflow-hidden">
-      <div className="flex-1 flex flex-col min-h-0">
-        <h1 className="text-2xl font-bold text-center mb-4 text-foreground flex-shrink-0">Mobile Image Viewer</h1>
-        <div className="flex-1 min-h-0">
+    <main className=" bg-background flex flex-col overflow-hidden h-screen">
+     
+       
+       
           <ZoomPanPinchViewer />
-        </div>
-      </div>
+      
+     
     </main>
   )
 }
